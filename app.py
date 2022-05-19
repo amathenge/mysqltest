e8e574c14cdc9d3081024077be3ff13559cc98ac
Andrew Mathenge <mathenge@gmail.com>
2022-05-19 21:49:58 +0300

    add python/flask files, index template

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import cred
+
+app = Flask(__name__)
+app.secret_key = cred.secret_key
+app.config['MYSQL_HOST'] = cred.mysql_host
+app.config['MYSQL_USER'] = cred.mysql_user
+app.config['MYSQL_PASSWORD'] = cred.mysql_password
+app.config['MYSQL_DB'] = cred.mysql_db
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute('select id, firstname, lastname, phone, email from users order by id desc')
+    result = cur.fetchall()
+    return render_template('index.html', data=result)
