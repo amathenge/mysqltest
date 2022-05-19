c7a7b53b7bdc70f808c207d0fb7c88348a6772f7
Andrew Mathenge <mathenge@gmail.com>
2022-05-19 22:46:07 +0300

    updated app.py, templates/index.html for invalid data

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ app.config['MYSQL_HOST'] = cred.mysql_host
 app.config['MYSQL_USER'] = cred.mysql_user
 app.config['MYSQL_PASSWORD'] = cred.mysql_password
 app.config['MYSQL_DB'] = cred.mysql_db
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
 @app.route('/')
 def home():
     cur = mysql.connection.cursor()
-    cur.execute('select id, firstname, lastname, phone, email from users order by id desc')
+    cur.execute('select id, firstname, lastname, phone, email from users order by id asc')
     result = cur.fetchall()
     return render_template('index.html', data=result)
